ddb5f2ed5a27fa0d8fd621d8bfb6fa4ccec89c2b
Tassio Marcos Rocha <tassiomarcosr@outlook.com>
2017-11-03 02:21:48 -0200

    refinamento no rodapé

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import footer from './footer.css';
+import Image from '../mini-components/image';
+import logo from '../../icons/LogoFull02.png';
 
 class Footer extends Component {
     render() {
@@ -7,13 +9,13 @@ class Footer extends Component {
             <footer>
                     <div className="row">
                         <div className="col-6">
-                            <h1>Musically 2017</h1>      
+                            <Image classe="footer-logo" photo={logo}/>      
                         </div>
-                        <div className="col-6">
-                            <p>Tássio Marcos - FullStack Developer</p>
+                        <div className="col-6 people">
+                            <p>Tássio Marcos - FullStack Developer & Design UX/UI</p>
                             <p>Fabiana Maria - Text Writer & Design UX/UI</p>
-                            <p>César Augusto - Front-End Developer</p>
-                            <p>Jonathan - Desig UX/UI</p>
+                            <p>César Augusto - Front-End Developer & Design UX/UI</p>
+                            <p>Jonathan Silva - Design UX/UI</p>
                         </div>
                     </div>
             </footer>
