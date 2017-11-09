a9f44804cf06adc2badbe0b31aa14a9eaec1704b
Tassio Marcos Rocha <tassiomarcosr@outlook.com>
2017-11-09 10:03:33 -0200

    Correção de bug login com facebook

--- src/tools/consts.js
+++ src/tools/consts.js
@@ -5,8 +5,6 @@ export const URL_LOGIN = 'https://musically.herokuapp.com/user/login';
 
 export const URL_EDIT_PHOTO = 'https://musically.herokuapp.com/user/photo/';
 
-export const URL_EDIT_SOCIAL = 'https://musically.herokuapp.com/user/socials/';
-
 export const URL_REGISTER_SOCIAL = 'https://musically.herokuapp.com/user/registerSocial';
 
 export const URL_LOGIN_SOCIAL = 'https://musically.herokuapp.com/user/auth';
@@ -21,10 +19,3 @@ export const URL_LOGIN_SOCIAL = 'https://musically.herokuapp.com/user/auth';
 // export const URL_LOGIN_SOCIAL = 'http://www.localhost:5000/user/auth';
 
 export const GLOBAL_USER = 'user';
-
-
-
-// export const URL_USER = 'https://musically-teste.herokuapp.com/user/';
-// export const URL_LOGIN = 'https://musically-teste.herokuapp.com/user/login';
-// export const URL_EDIT_PHOTO = 'http://www.localhost:5000/user/photo/';
-// export const URL_EDIT_SOCIAL = 'http://www.localhost:5000/user/socials/';
