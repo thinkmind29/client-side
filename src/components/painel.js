a8993a02a51f9c7e9a1266b39ec5877813eccc2c
Tassio Marcos Rocha <tassiomarcosr@outlook.com>
2017-11-29 17:47:57 -0200

    >

--- src/components/painel.js
+++ src/components/painel.js
@@ -6,11 +6,11 @@ const Painel = ({ item, onItemPress}) => {
 
     const { name, photo, age, hability, city, state, _id } = item;
     return <li className="painel" onClick={() => onItemPress(item._id, item.name)}>
-                <div className="row" >       
+                <div className="row">       
                     <div className="col-2 col-sm-2" >
                         <Image photo={photo} classe="badge" />
                     </div>
-                    <div className="col-8 col-sm-8">
+                    <div className="col-8 col-sm-8 text-painel">
                         <p> {name} </p>
                         <p className="atributos"> {age} anos, {hability}</p>
                         <p className="atributos"> {city}, {state}</p>
